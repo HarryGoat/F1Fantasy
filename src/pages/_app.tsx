3348049efd32a1873fd9807b178a0fba770375ac
Harry Goatcher <goatcherhf@gmail.com>
2024-02-07 14:31:30 +0000

    Add redirect after sign up, Add delete user from db

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 import { ClerkProvider, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
@@ -12,19 +11,9 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <Component {...pageProps} />
         <main>
         <div className="fixed bottom-0 left-0 z-50 w-full border-t border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700">
-    <div className="mx-auto grid h-full max-w-lg grid-cols-6">
-      {/* Sign In */}
-      <div className="dark:hover-bg-gray-800 group inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 col-span-1">
-        <SignInButton>
-          <svg
-            className="mb-1 h-5 w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
-            fill="currentColor"
-          >
-            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-          </svg>
-        </SignInButton>
-      </div>
-
+            <div className="mx-auto grid h-full max-w-lg grid-cols-6">
+             {/* Sign In Button */}
+             <SignInOrOutButton />
               <Link
                 href="/races"
                 className="dark:hover-bg-gray-800 group inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50"
@@ -99,16 +88,53 @@ const MyApp: AppType = ({ Component, pageProps }) => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
-                  />
+                    d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
                 </svg>
                 <span className="sr-only">settings</span>
               </Link>
             </div>
-          </div>
+            </div>
         </main>
       </ClerkProvider>
     </>
+  );
+};
+
+const SignInOrOutButton = () => {
+  const { isSignedIn } = useUser();
+  return (
+    <div className="dark:hover-bg-gray-800 group inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 col-span-1">
+      {isSignedIn ? (
+        <SignOutButton>
+          <svg
+            className="mb-1 h-5 w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+            fill="currentColor"
+          >
+            <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+          </svg>
+        </SignOutButton>
+      ) : (
+        <SignInButton>
+          <svg
+            className="mb-1 h-5 w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+            fill="currentColor"
+          >
+            <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
+          </svg>
+        </SignInButton>
+      )}
+    </div>
   );
 };
 
--- src/pages/api/webhooks/user.ts
+++ src/pages/api/webhooks/user.ts
@@ -4,6 +4,8 @@ import { type NextApiRequest, type NextApiResponse } from 'next';
 import { buffer } from 'micro';
 import { db } from '~/server/db';
 import { user } from '~/server/db/schema';
+import { eq } from 'drizzle-orm';
+import z from "zod";
  
 export const config = {
   api: {
@@ -16,8 +18,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(405).end();
     return;
   }
+  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
  
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
@@ -28,13 +31,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const svix_timestamp = req.headers["svix-timestamp"] as string;
   const svix_signature = req.headers["svix-signature"] as string;
  
- 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return res.status(400).json({ error: 'Error occured -- no svix headers' })
   }
  
   console.log('headers', req.headers, svix_id, svix_signature, svix_timestamp)
+  
   // Get the body
   const body = (await buffer(req)).toString()
  
@@ -56,30 +59,38 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
  
   // Get the ID and type
-  const { id } = evt.data;
+  const id = z.string().parse(evt.data.id);
   const eventType = evt.type;
- 
+
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   console.log('Webhook body:', body)
 
-  if (eventType == "user.created"){
+  if (eventType === "user.created"){
     try {
-      // Insert the user data into your Drizzle-managed MySQL database
       await db.insert(user).values({
-        id: evt.data.id,
+        id: id,
         userName: evt.data.username,
         budget: 100,
         driverChange: false,
       });
-
-
-      res.redirect('/');
     } catch (error) {
       console.error('Error inserting user data into Drizzle database:', error);
     }
-   
   }
- 
+  else if (eventType === "user.deleted"){
+    try {
+      await db.delete(user).where(eq(user.id, id));
+    } catch (error) {
+      console.error('Error deleting user data from Drizzle database:', error);
+    }
+  }
+  redirect(res);
   res.status(200).end();
 }
- 
+
+
+function redirect(res: NextApiResponse): void {
+  res.setHeader('Location', '/');
+  res.statusCode = 302;
+  res.end();
+}
